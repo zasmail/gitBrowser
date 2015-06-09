991debc71eb209c13c185a30a25ada438602bb4a
Zo Asmail <zasmail@utexas.edu>
2015-06-09 00:14:38 -0700

    Tried to access the meta info, added the OAUTH key and tried to get it into the environment folder

--- app/routes/endpoints.js
+++ app/routes/endpoints.js
@@ -1,13 +1,24 @@
 import Ember from 'ember';
 import Endpoint from 'git-sandbox/models/endpoint';
+//import Settings from 'git-sandbox/config/environment.js';
 
 var EndpointsRoute = Ember.Route.extend({
   model: function() {
     return Ember.$.ajax({
       type: 'GET',
       url: 'https://api.github.com/',
-      dataType: 'jsonp'
+      dataType: 'jsonp',
+      //data: { access_token: Settings.OAUTH.GIT }
+      data: { access_token: '551d0c04a362d8c84c33ff6b6177ff13fec80607' }
+    //}).then(function(data){
+    //  var rate =  data.meta["X-RateLimit-Remaining"];
+    //  debugger
     }).then(function(data){
+      //var rate =  data.meta["X-RateLimit-Remaining"];
+      //debugger
+      //can't get around function scope here
+      //Bad solution: inject rate into every element- I tried this but couldn't figure out how to circumvent the function scope
+      //Good solution: I think a second then which handles the meta data- when I tried this, the concept of data was data.data
       return Ember.keys(data.data).map ( function (element){
         var name = element;
         var url  = data.data[name];
