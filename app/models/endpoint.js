7498902bba06dacca68426ab22c27bc50d4387a5
Zo Asmail <zasmail@utexas.edu>
2015-06-09 01:11:59 -0700

    Started work on dynamic links

--- app/models/endpoint.js
+++ app/models/endpoint.js
@@ -3,6 +3,19 @@ import Ember from 'ember';
 var EndpointModel = Ember.Object.extend({
   url: undefined,
   name: undefined,
+
+  uri: Ember.computed('url', function(){
+    var dynamicUri = this.get('url');
+    var dynamic = dynamicUri.split("{").map(function(element){return element;});
+    var first = dynamic[0];// WHY IS THERE NO POLL FX
+    dynamic.shift();
+    dynamic.forEach(function(name){
+      dynamic[name] += "{";
+      //this isn't doing anything- more problems with function scope
+    });
+    var uri = first + dynamic.join("{");
+    return uri;
+  }),
   englishName: Ember.computed('name', function(){
     var name = this.get('name');
     var english = name.split("_").map(function(element){return element.capitalize(); });
