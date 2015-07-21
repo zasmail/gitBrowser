3072c396cf262ebe0bce910f04162a09e493b8f7
Zo Asmail <zasmail@utexas.edu>
2015-07-21 11:58:35 -0700

    Url segments are now editable

--- app/components/url-segment.js
+++ app/components/url-segment.js
@@ -4,13 +4,25 @@ var urlSegment = Ember.Component.extend({
   tagName: 'span',
   classNameBindings: ['isDynamic:dynamic:static'],
   content: undefined,
+  other: "other",
+  isInputting: false,
   isDynamic: Ember.computed('content', function(){
     if(this.get('content').charAt(0)==="{") {
       return true;
     }else {
       return false;
     }
-  })
+  }),
+
+  actions: {
+    startInputting: function (){
+      this.set('isInputting', true);
+    },
+
+    stopInputting: function (){
+      this.set('isInputting', false);
+    }
+  }
 });
 
 export default urlSegment;
